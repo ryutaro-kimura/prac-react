658325f252b7b24093834f6d4f8b7edbfd2384c7
ryutaro-kimura <sasuker2264@gmail.com>
2022-01-17 14:02:57 +0900

    feat: useStateを用いて、状態の変更機能を実装

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { useState } from 'react';
 import { List } from './List';
 
 function App() {
+  const [description, setDescription] = useState('クリック前の表示');
+  const changeDescrioption = () => {
+    setDescription('クリック後の表示')
+  }
   return (
     <div>
-      タイトル
+      { description }
       <List title="取扱言語一覧"/>
+      <button onClick={() => changeDescrioption()}>ボタン</button>
     </div>
   );
 }
