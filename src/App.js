92d5644dd285ee5ecefffed5aa30b6857a28b4bf
ryutaro-kimura <sasuker2264@gmail.com>
2022-01-17 14:18:13 +0900

    feat: 条件分岐でフォームとリストの表示切り替え

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import { useState } from 'react';
 import { List } from './List';
+import { Form } from './Form';
 
 function App() {
   const [description, setDescription] = useState('クリック前の表示');
+  const [tab, setTab] = useState('list');
   const changeDescrioption = () => {
     setDescription('クリック後の表示')
   }
+
   return (
     <div>
+      <header>
+        <ul>
+          <li onClick={() => setTab('list')}>リスト</li>
+          <li onClick={() => setTab('form')}>フォーム</li>
+        </ul>
+      </header>
+      <hr/>
       { description }
-      <List title="取扱言語一覧"/>
+      {
+        tab === 'list' ? <List title="取扱言語一覧" /> : <Form />
+      }
       <button onClick={() => changeDescrioption()}>ボタン</button>
     </div>
   );
--- src/List.js
+++ src/List.js
@@ -2,7 +2,6 @@ export const List = (props) => {
     const title = props.title;
     return (
         <div>
-            <h4>{ title }</h4>
             <div>リストです</div>
         </div>
     )
